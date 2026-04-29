a175728978d3fc9306a4b047ff6b7e185cfe7578
Charles Reid <charlesreid1@gmail.com>
2026-04-28 20:16:22 -0700

    fix up Day-01 perl solution

--- Day-01/Perl/solution.pl
+++ Day-01/Perl/solution.pl
@@ -1,5 +1,11 @@
-$_=q[0123456789];$p=ord(substr$_,2,1);$c=0;while(<>){/^([LR])(\d+)$/ or next;
-$p=($p+($1eq"R"?$2:-$2))%100;$c+=!$p}print"$c\n";#---------------------------
-#The dial turns left/right,counting zeroes.Fifty starts,modulo hundred.####
-#R adds,L subtracts,zero increments.Answer from arithmetic dance.Perl#####
-#magic obscures simple truth.--------------------------------------------#
+# the dial turns 
+# left and right
+# counting zeros
+# fifty starts, mod a hundred
+# R adds L, subtracts, zero increments
+# answer from arithmetic dance
+# magic obscures simple truth
+$_=q[0123456789];$p=ord(substr$_,2,1);
+$c=0;while(<>){/^([LR])(\d+)$/ or next;
+$p=($p+($1eq"R"?$2:-$2))%100;
+$c+=!$p}print"$c\n";
